ac263c2cd0b70e8131fb1283f20179bde63cd1bc
AntonVoronezh <pstut@mail.ru>
2019-05-04 18:17:47 +0300

    add setBooksAC

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store/store';
 
+
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={store()}>
         <App />
     </Provider>
 , document.getElementById('root'));
--- src/store/reducers/booksReducer.js
+++ src/store/reducers/booksReducer.js
@@ -1,6 +1,13 @@
+const SET_BOOKS = 'SET_BOOKS';
+const ADD_BOOK = 'ADD_BOOK';
+
 export default (state = {}, action) => {
 	switch (action.type) {
 		default:
 			return state;
 	}
 };
+
+export const setBooksAC = books => ({type: SET_BOOKS, payload:{books}})
+
+ 
