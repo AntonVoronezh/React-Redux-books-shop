33be2c9ac3779bcf93fe0a2fee7d5d5e87314d38
AntonVoronezh <pstut@mail.ru>
2019-05-04 18:10:39 +0300

    add logger to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import rootReducer from './reducers/rootReducer';
 
-
-export default () => createStore(rootReducer, applyMiddleware);
+export default () => createStore(rootReducer, applyMiddleware(logger));
