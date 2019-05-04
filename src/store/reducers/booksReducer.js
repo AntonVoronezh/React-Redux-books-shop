31b3fe5f4d4515c4a2abf63f8302b465aeeadc4b
AntonVoronezh <pstut@mail.ru>
2019-05-04 18:37:27 +0300

    add case SET_BOOKS, ADD_BOOK

--- src/store/reducers/booksReducer.js
+++ src/store/reducers/booksReducer.js
@@ -2,12 +2,22 @@ const SET_BOOKS = 'SET_BOOKS';
 const ADD_BOOK = 'ADD_BOOK';
 // booksLvl
 const initialState = {
-    books:[],
-    aaaaa: 'что0то еще'
-}
+	books: [],
+	aaaaa: 'что0то еще',
+};
 
 export default (state = initialState, action) => {
 	switch (action.type) {
+		case SET_BOOKS:
+			return {
+				...state,
+				books: action.payload.books,
+			};
+		case ADD_BOOK:
+			return {
+				...state,
+				books: [...state.books, action.payload.book],
+			};
 		default:
 			return state;
 	}
