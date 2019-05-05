ab8573fffbdcf049e3f818556be2e49087c3c42d
AntonVoronezh <pstut@mail.ru>
2019-05-05 17:56:25 +0300

    add key

--- src/components/Books/Book/Book.jsx
+++ src/components/Books/Book/Book.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default props => {
+    debugger
     return (
         <div>
-            Books
+            Book
         </div>
         
     )
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Book from './Book/Book';
 
-export default ({ books, isLoading }) => {
-    return (
-        <div>
-            <div className="App">{isLoading ? 'загрузка...' : '+++' + books}</div>;
-        </div>
-        
-    )
-}
+export default ({ books: array, isLoading }) => {
+    let books;
+
+	if (!isLoading) {
+		books = array.map(b => {
+			return <Book key={b.id} {...b}/>;
+		});
+	}
+
+	return (
+		<div>
+			<div className="App">{isLoading ? 'загрузка...' : books}</div>;
+		</div>
+	);
+};
