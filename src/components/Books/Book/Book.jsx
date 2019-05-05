b32c5ad27714eca33c4ef7b86564dac36fa5e985
AntonVoronezh <pstut@mail.ru>
2019-05-05 18:09:06 +0300

    add markup in Book

--- src/components/Books/Book/Book.jsx
+++ src/components/Books/Book/Book.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image } from 'semantic-ui-react';
 
-export default props => {
-    return (
-        <div>
-            Book
-        </div>
-        
-    )
-}
+export default ({ image, title, author, price }) => {
+	return (
+		<Card>
+			<Image src={image} />
+			<Card.Content>
+				<Card.Header>{title}</Card.Header>
+				<Card.Meta>
+					<span className="date">{author}</span>
+				</Card.Meta>
+			</Card.Content>
+			<Card.Content extra>
+				<Icon name="rub" />
+				{price} руб.
+			</Card.Content>
+		</Card>
+	);
+};
