d9d7b9e0f941503802a5333acc022dc5ffeb025c
AntonVoronezh <pstut@mail.ru>
2019-05-05 19:05:36 +0300

    refactoring Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,12 +4,17 @@ import { Menu } from 'semantic-ui-react';
 export default () => {
 	return (
 		<Menu>
-			<Menu.Item name="browse">Browse</Menu.Item>
+			<Menu.Item name="browse">
+				<img
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFBCU7SIBvnFfTT1mq5sKSiGJcjYKbAOYJio71iNsMn_OG6pgJIA"
+					alt="dd"
+				/>
+			</Menu.Item>
 
 			<Menu.Menu position="right">
-				<Menu.Item name="signup">Sign Up</Menu.Item>
+				<Menu.Item name="signup">Итого</Menu.Item>
 
-				<Menu.Item name="help"> Help</Menu.Item>
+				<Menu.Item name="help"> Корзина</Menu.Item>
 			</Menu.Menu>
 		</Menu>
 	);
