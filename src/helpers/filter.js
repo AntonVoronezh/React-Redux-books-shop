c3a89b279911a286788e676e42371d70115928d7
AntonVoronezh <pstut@mail.ru>
2019-05-06 19:43:45 +0300

    mounting orderBy

--- src/helpers/filter.js
+++ src/helpers/filter.js
@@ -1,1 +1,16 @@
-import lodash from 'lodash'
+import orderBy from 'lodash/orderBy';
+
+export default (array, type) => {
+	switch (type) {
+		case 'all':
+			return array;
+		case 'byPriceExpansive':
+			return orderBy(array, 'price', 'desc');
+		case 'byPriceCheap':
+			return orderBy(array, 'price', 'asc');
+		case 'author':
+			return orderBy(array, 'author', 'desc');
+		default:
+			return array;
+	}
+};
