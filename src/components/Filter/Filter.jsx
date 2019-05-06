be2837d6655430835232ca179f902d3658750e12
AntonVoronezh <pstut@mail.ru>
2019-05-06 17:28:31 +0300

    add active in Filter component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
-export default ({activeItem, changeActiveItemFilterCB}) => {
-    
-    const handleItemClick = item => {
-        changeActiveItemFilterCB(item);
-    }
+export default ({ activeItem, changeActiveItemFilterCB }) => {
+	const handleItemClick = item => {
+		changeActiveItemFilterCB(item);
+	};
 
 	return (
 		<Menu secondary>
 			<Menu.Menu>
-				<Menu.Item name="all" onClick={handleItemClick.bind(null, 'all')}>Все</Menu.Item>
-				<Menu.Item name="byPriceExpansive"> По цене (дорогие)</Menu.Item>
-				<Menu.Item name="byPriceCheap"> По цене (дешевые)</Menu.Item>
-				<Menu.Item name="byAutor"> По автору</Menu.Item>
+				<Menu.Item name="all" active={activeItem === 'all'} onClick={handleItemClick.bind(null, 'all')}>
+					Все
+				</Menu.Item>
+				<Menu.Item
+					name="byPriceExpansive"
+					active={activeItem === 'byPriceExpansive'}
+					onClick={handleItemClick.bind(null, 'byPriceExpansive')}
+				>
+					По цене (дорогие)
+				</Menu.Item>
+				<Menu.Item
+					name="byPriceCheap"
+					active={activeItem === 'byPriceCheap'}
+					onClick={handleItemClick.bind(null, 'byPriceCheap')}
+				>
+					По цене (дешевые)
+				</Menu.Item>
+				<Menu.Item
+					name="byAutor"
+					active={activeItem === 'byAutor'}
+					onClick={handleItemClick.bind(null, 'byAutor')}
+				>
+					По автору
+				</Menu.Item>
 			</Menu.Menu>
 		</Menu>
 	);
