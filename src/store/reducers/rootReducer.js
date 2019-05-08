64b83d925d55998d5f49159d3fbc657fd33513ac
AntonVoronezh <pstut@mail.ru>
2019-05-08 09:18:34 +0300

    add in rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import booksReducer from './booksReducer';
 import uiReduser from './uiReduser';
+import cardReducer from './cardReducer';
 
 export default combineReducers({
 	booksLvl: booksReducer,
 	ui: uiReduser,
+	card: cardReducer,
 });
