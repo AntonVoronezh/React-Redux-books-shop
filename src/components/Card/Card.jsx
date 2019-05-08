60bc1d2c5e4c70ab534729950e194f96fc2179cb
AntonVoronezh <pstut@mail.ru>
2019-05-08 12:43:02 +0300

    add listCard

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
-import { Menu, Popup } from 'semantic-ui-react';
+import { Menu, Popup, List, Image } from 'semantic-ui-react';
 
 export default ({ list, total, deleteFromCardCB }) => {
+	const listCard = list.map(b => {
+        return (
+            <List.Item>
+            <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+            <List.Content>
+              <List.Header as='a'>Rachel</List.Header>
+              <List.Description>
+                Last seen watching{' '}
+                <a>
+                  <b>Arrested Development</b>
+                </a>{' '}
+                just now.
+              </List.Description>
+            </List.Content>
+          </List.Item> 
+        )
+    });
+
 	return (
 		<React.Fragment>
 			<Menu.Item name="signup">Итого {total} руб.</Menu.Item>
-			<Menu.Item name="help"> Корзина ({list.length})</Menu.Item>
+			<Popup
+				trigger={<Menu.Item name="help"> Корзина ({list.length})</Menu.Item>}
+				header="Movie Search"
+				content={<List />}
+				on={['hover', 'click']}
+			/>
 		</React.Fragment>
 	);
 };
 
-// <Popup
-// trigger={<Button icon>Click me or Hover me</Button>}
-// header='Movie Search'
-// content='Multiple events can trigger a popup'
-// on={['hover', 'click']}
-// />
+{
+	/* <List.Item>
+      <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
+      <List.Content>
+        <List.Header as='a'>Rachel</List.Header>
+        <List.Description>
+          Last seen watching{' '}
+          <a>
+            <b>Arrested Development</b>
+          </a>{' '}
+          just now.
+        </List.Description>
+      </List.Content>
+    </List.Item> */
+}
