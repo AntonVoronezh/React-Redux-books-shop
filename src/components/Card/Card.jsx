d5d9e1329ced466f7383da1d1ebd84e927346cd5
AntonVoronezh <pstut@mail.ru>
2019-05-08 12:49:27 +0300

    refactoring listCard

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import { Menu, Popup, List, Image } from 'semantic-ui-react';
+import { Menu, Popup, List, Image, Button } from 'semantic-ui-react';
 
 export default ({ list, total, deleteFromCardCB }) => {
 	const listCard = list.map(b => {
-        return (
-            <List.Item>
-            <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
-            <List.Content>
-              <List.Header as='a'>Rachel</List.Header>
-              <List.Description>
-                Last seen watching{' '}
-                <a>
-                  <b>Arrested Development</b>
-                </a>{' '}
-                just now.
-              </List.Description>
-            </List.Content>
-          </List.Item> 
-        )
-    });
+		return (
+			<List.Item>
+				<Image avatar src={b.image} />
+				<List.Content>
+					<List.Header as="a">{b.title}</List.Header>
+					<List.Description>{b.author}</List.Description>
+				</List.Content>
+			</List.Item>
+		);
+	});
 
 	return (
 		<React.Fragment>
 			<Menu.Item name="signup">Итого {total} руб.</Menu.Item>
 			<Popup
 				trigger={<Menu.Item name="help"> Корзина ({list.length})</Menu.Item>}
-				header="Movie Search"
-				content={<List />}
+				header="Ничего нет"
+				content={<List>{listCard}</List>}
 				on={['hover', 'click']}
 			/>
 		</React.Fragment>
