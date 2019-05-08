532d69fc564ceb9724b668996f5831fc7b31f39e
AntonVoronezh <pstut@mail.ru>
2019-05-08 12:56:03 +0300

    finish

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -19,26 +19,10 @@ export default ({ list, total, deleteFromCardCB }) => {
 			<Menu.Item name="signup">Итого {total} руб.</Menu.Item>
 			<Popup
 				trigger={<Menu.Item name="help"> Корзина ({list.length})</Menu.Item>}
-				header="Ничего нет"
 				content={<List>{listCard}</List>}
-				on={['hover', 'click']}
 			/>
 		</React.Fragment>
 	);
 };
 
-{
-	/* <List.Item>
-      <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png' />
-      <List.Content>
-        <List.Header as='a'>Rachel</List.Header>
-        <List.Description>
-          Last seen watching{' '}
-          <a>
-            <b>Arrested Development</b>
-          </a>{' '}
-          just now.
-        </List.Description>
-      </List.Content>
-    </List.Item> */
-}
+
